31d528b9f7f9ace5728a03388974b15f431f8cc2
Andrew Lawson <aeriklawson@gmail.com>
2016-07-09 19:49:46 -0400

    Made a few changes for testing.

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 export default class Board extends React.Component {
 	render() {
 		return (
-			<p>Board!</p>
+			<div>
+				<p>Board!</p>
+			</div>
 		);
 	}
 }
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -3,7 +3,7 @@ import Board from './board.jsx';
 import Tile from './tile.jsx';
 import {render} from 'react-dom';
 
-class App extends React.Component {
+export default class App extends React.Component {
 	render() {
 		return (
 			<div>
