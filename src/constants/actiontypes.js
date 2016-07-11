f4e717ddac22a889f335b428531ec206989dc8f0
Andrew Lawson <aeriklawson@gmail.com>
2016-07-10 22:26:04 -0400

    Separate types into action and game. Add more logic to reducer.

--- src/constants/actiontypes.js
+++ src/constants/actiontypes.js
@@ -1,5 +1,3 @@
 export const MAKE_MOVE = 'MAKE_MOVE';  
 export const RESET_BOARD = 'RESET_BOARD';  
 export const NEW_GAME = 'NEW_GAME';
-export const PLAYER_ONE = 'X';
-export const PLAYER_TWO = 'O';
--- src/reducers/reducer.js
+++ src/reducers/reducer.js
@@ -1,11 +1,14 @@
-import * as Types from '../constants/actiontypes';
+import * as ActionTypes from '../constants/actiontypes';
+import * as GameTypes from '../constants/gametypes';
+import _ from 'lodash';
 
 const initialState = {
-	board: createBoard(3),
-	boardDim: null,
-	currentMatch: null,
-	numMatches: 3,
-	currentPlayer: null
+	board: null,
+	boardDim: 0,
+	currentMatch: 0,
+	numMatches: 0,
+	currentPlayer: GameTypes.PLAYER_ONE,
+	gameStatus: GameTypes.NOT_WINNER
 };
 
 function createBoard(boardDim) {
@@ -26,12 +29,27 @@ function makeMoveOnBoard(board, player, tileId) {
 	return;
 }
 
+function isWinner(board, player) {
+	return GameTypes.NOT_WINNER;
+}
+
 export default function gameInfo(state=initialState, action) {
 	switch(action.type) {
-		case Types.MAKE_MOVE:
-			// TODO: Add logic for making a move.
-			return state;
-		case Types.RESET_BOARD:
+		case ActionTypes.MAKE_MOVE:
+			const tileId = action.payload.tileId;
+			const player = action.payload.player;
+			const board = _.cloneDeep(state.board);
+			board[tileId] = player;
+			const result = isWinner(board, player);
+			return {
+				board: board,
+				boardDim: state.boardDim,
+				currentPlayer: state.currentPlayer,
+				currentMatch: state.currentMatch,
+				numMatches: state.numMatches,
+				gameStatus: result
+			};
+		case ActionTypes.RESET_BOARD:
 			return {
 				board: createBoard(state.boardDim),
 				boardDim: state.boardDim,
@@ -39,7 +57,7 @@ export default function gameInfo(state=initialState, action) {
 				currentMatch: state.currentMatch + 1,
 				numMatches: state.numMatches
 			};
-		case Types.NEW_GAME:
+		case ActionTypes.NEW_GAME:
 			return {
 				board: createBoard(action.boardDim),
 				boardDim: action.boardDim,
