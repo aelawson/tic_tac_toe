30241df237689edcc017b393d457b21e1b338453
Andrew Lawson <aeriklawson@gmail.com>
2016-07-12 13:00:56 -0400

    Add more reducer tests.

--- test/reducers/reducer.reducer.spec.js
+++ test/reducers/reducer.reducer.spec.js
@@ -1,5 +1,6 @@
 import chai from 'chai';
 
+import * as GameTypes from '../../src/constants/gametypes';
 import * as reducers from '../../src/reducers/reducer';
 
 describe('Reducers', () => {
@@ -9,6 +10,15 @@ describe('Reducers', () => {
 			const rows = reducers.createBoard(boardDim);
 		    chai.expect(rows).to.have.length.of(10);
 		    chai.expect(rows[0]).to.have.length.of(10);
+		});
+	});
+
+	describe('isWinner()', () => {
+		it('should return a non-winning result given the board and player"', () => {
+			const expectedResult = GameTypes.NOT_WINNER;
+			const board = reducers.createBoard(5);
+			const actualResult = reducers.isWinner(board, GameTypes.PLAYER_ONE);
+			chai.expect(actualResult).to.equal(expectedResult);
 		});
 	});
 });
