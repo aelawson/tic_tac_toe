cf80d4bc1ebdf0505a6fb0aaad24cc7801daf8e1
Andrew Lawson <aeriklawson@gmail.com>
2016-07-13 18:26:35 -0400

    Add working endgame screen - needs button to reset game.

--- src/components/board.jsx
+++ src/components/board.jsx
@@ -1,7 +1,16 @@
 import React from 'react';
 import Tile from './tile';
+import * as GameTypes from '../constants/gametypes';
 
 require('./css/board.css');
+
+const hideEndGame = {
+	display: "none"
+};
+
+const showEndGame = {
+	display: "block"
+};
 
 export function getTileSize(boardDim) {
 	var marginSize = 10;
@@ -11,11 +20,31 @@ export function getTileSize(boardDim) {
 	return tileSize;
 };
 
+export function setEndGameStyle(matchStatus) {
+	if (matchStatus === GameTypes.WINNER) {
+		return showEndGame;
+	}
+	if (matchStatus === GameTypes.DRAW) {
+		return showEndGame;
+	}
+	return hideEndGame;
+};
+
 export default class Board extends React.Component {
 	render() {
 		const tileSize = getTileSize(this.props.boardDim);
+		//const startGameStyle = setstartGameStyle(this.props.matchStatus);
+		const endGameStyle = setEndGameStyle(this.props.matchStatus);
 		return (
 			<div id="board">
+				<div
+					id="endGameScreen"
+					style={endGameStyle}
+				>
+					<h2>
+						{this.props.matchStatus}
+					</h2>
+				</div>
 				{
 					// Generate tiles for board.
 					this.props.board.map((value, index) => {
