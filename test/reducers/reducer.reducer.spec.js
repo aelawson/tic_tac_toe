78f4e346ba4f628b98719b5a4974d0ca9c892d93
Andrew Lawson <aeriklawson@gmail.com>
2016-07-13 23:22:31 -0400

    Add more tests - TODO.

--- test/reducers/reducer.reducer.spec.js
+++ test/reducers/reducer.reducer.spec.js
@@ -1,33 +1,23 @@
 import chai from 'chai';
 
 import * as GameTypes from '../../src/constants/gametypes';
-import * as reducers from '../../src/reducers/reducer';
-import * as Helpers from '../../src/helpers/reducerhelpers';
+import reducer from '../../src/reducers/reducer';
 
 describe('Reducers', () => {
-	describe('Helpers', () => {
-		describe('createBoard()', () => {
-			it('should return a 1D, flattened array of size "boardDim"^2', () => {
-				const boardDim = 10;
-				const board = Helpers.createBoard(boardDim);
-			    chai.expect(board).to.have.length.of(boardDim * boardDim);
-			});
-		});
-
-		describe('isWinner()', () => {
-			it('should return a winning result given a winning play"', () => {
-				const expectedResult = GameTypes.WINNER;
-				const board = [ "X", "X", "X", "O", "O", null, null, null, null ];
-				const actualResult = Helpers.isWinner(board, 3, 2, GameTypes.PLAYER_ONE);
-				chai.expect(actualResult).to.equal(expectedResult);
-			});
-
-			it('should return a non-winning result given a non-winning play"', () => {
-				const expectedResult = GameTypes.NOT_WINNER;
-				const board = [ "X", "X", null, "O", "O", null, null, null, null ]
-				const actualResult = Helpers.isWinner(board, 3, 4, GameTypes.PLAYER_TWO);
-				chai.expect(actualResult).to.equal(expectedResult);
-			});
-		});
+	it('should return expected state for a given MAKE_MOVE action"', () => {
+		// TODO: Implement test.
+		chai.expect(true).to.equal(true);
+	});
+	it('should return expected state for a given RESET_BOARD action"', () => {
+		// TODO: Implement test.
+		chai.expect(true).to.equal(true);
+	});
+	it('should return expected state for a given NEW_GAME action"', () => {
+		// TODO: Implement test.
+		chai.expect(true).to.equal(true);
+	});
+	it('should return current state for an unmatched (default) action"', () => {
+		// TODO: Implement test.
+		chai.expect(true).to.equal(true);
 	});
 });
